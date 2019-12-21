15e33b5a6e5b264c00784077fbad5c60fa814bb4
Terrell Gentry <terrellgentry@hotmail.com>
2019-12-21 05:50:55 -0800

    Update create-top-level-oauth-redirect.ts

--- src/auth/create-top-level-oauth-redirect.ts
+++ src/auth/create-top-level-oauth-redirect.ts
@@ -7,9 +7,9 @@ import {TOP_LEVEL_OAUTH_COOKIE_NAME} from './index';
 export default function createTopLevelOAuthRedirect(path: string) {
   return async function topLevelOAuthRedirect(ctx: Context) {
     ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME, '1');
-    setTimeout(() => {
-      const redirect = createTopLevelRedirect(path);
+    //setTimeout(() => {
+      //const redirect = createTopLevelRedirect(path);
       //redirect(ctx);
-    }, 75);
+    //}, 75);
   };
 }
