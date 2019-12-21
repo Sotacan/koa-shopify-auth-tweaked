f0b63de9697c4aaf49cfa9a414306ac2dfbce047
Terrell Gentry <terrellgentry@hotmail.com>
2019-12-20 21:21:29 -0800

    initial commit

---
+++ src/auth/create-top-level-oauth-redirect.ts
@@ -0,0 +1,15 @@
+import {Context} from 'koa';
+
+import createTopLevelRedirect from './create-top-level-redirect';
+
+import {TOP_LEVEL_OAUTH_COOKIE_NAME} from './index';
+
+export default function createTopLevelOAuthRedirect(path: string) {
+  return async function topLevelOAuthRedirect(ctx: Context) {
+    ctx.cookies.set(TOP_LEVEL_OAUTH_COOKIE_NAME, '1');
+    setTimeout(() => {
+      const redirect = createTopLevelRedirect(path);
+      redirect(ctx);
+    }, 75);
+  };
+}
