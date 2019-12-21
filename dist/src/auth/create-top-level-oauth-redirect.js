c0c0b79dffbc170f3a9fa11c16c9bae51b9dc63e
Terrell Gentry <terrellgentry@hotmail.com>
2019-12-21 05:50:27 -0800

    Update create-top-level-oauth-redirect.js

--- dist/src/auth/create-top-level-oauth-redirect.js
+++ dist/src/auth/create-top-level-oauth-redirect.js
@@ -6,10 +6,10 @@ var index_1 = require("./index");
 function createTopLevelOAuthRedirect(path) {
     return function topLevelOAuthRedirect(ctx) {
         ctx.cookies.set(index_1.TOP_LEVEL_OAUTH_COOKIE_NAME, '1');
-        setTimeout(() => {
-            var redirect = create_top_level_redirect_1.default(path);
+        //setTimeout(() => {
+            //var redirect = create_top_level_redirect_1.default(path);
             //redirect(ctx);
-        }, 75);
+        //}, 75);
     };
 }
 exports.default = createTopLevelOAuthRedirect;
